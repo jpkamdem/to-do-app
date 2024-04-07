e3c88076e476cbbc6d6eeff5be2a6f3d6034f7dc
jpkamdem <jpkamdem@proton.me>
2024-04-07 14:08:42 +0200

    bouton suppression fini

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,21 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  const handleDelete = (item) => {
+    console.log("Deleted", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  };
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteBtn}>x</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteBtn}
+          >
+            x
+          </button>
         </span>
         <hr className={styles.line} />
       </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,23 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <>
       {todos.length > 0 ? (
-      <div className={styles.list}>
-        {todos.map((index) => (
-          <TodoItem key={index} item={index} />
-        ))}
-      </div>
-      ) : ('')}
+        <div className={styles.list}>
+          {todos.map((index) => (
+            <TodoItem
+              key={index}
+              item={index}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
