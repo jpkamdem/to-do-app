e7ae68ba80d2ad448a1d2beeea98f4805a9bcde3
jpkamdem <jpkamdem@proton.me>
2024-04-08 14:32:27 +0200

    tri des todos, app terminée

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,15 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
+
   return (
     <>
-      {todos.length > 0 ? (
+      {sortedTodos.length > 0 ? (
         <div className={styles.list}>
-          {todos.map((item) => (
+          {sortedTodos.map((item) => (
             <div key={item.name}>
               <TodoItem item={item} todos={todos} setTodos={setTodos} />
             </div>
